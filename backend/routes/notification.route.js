db3dab01139435d563f8f335e0169348ae994bf5
Halyna Danylko <danilkogallina@gmail.com>
2024-12-09 12:37:05 +0200

    add notification routes and controllers

---
+++ backend/routes/notification.route.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { protectRoute } from "../middleware/protectRoute.js";
+import {
+  deleteNotifications,
+  getNotificatons,
+  deleteNotification,
+} from "../controllers/notifications.controller.js";
+
+const router = express.Router();
+
+router.get("/", protectRoute, getNotificatons);
+router.delete("/", protectRoute, deleteNotifications);
+router.delete("/:id", protectRoute, deleteNotification);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import { v2 as cloudinary } from "cloudinary";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
+import notificationRoutes from "./routes/notification.route.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
@@ -26,8 +27,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/posts", postRoutes);
-// app.use("/api/auth", authRoutes);
-// app.use("/api/auth", authRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
