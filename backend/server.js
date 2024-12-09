fe19bc7c2742d335f1ad525300047abe79530a7d
Halyna Danylko <danilkogallina@gmail.com>
2024-12-09 11:49:59 +0200

    post routers and controllers

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import { v2 as cloudinary } from "cloudinary";
 
 import userRoutes from "./routes/user.routes.js";
 import authRoutes from "./routes/auth.routes.js";
+import postRoutes from "./routes/post.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
@@ -24,7 +25,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
-// app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
 // app.use("/api/auth", authRoutes);
 // app.use("/api/auth", authRoutes);
 
