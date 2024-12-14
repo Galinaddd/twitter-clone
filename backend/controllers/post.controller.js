313ede1ad7b38dcb94aa2b3b85f408bb10d1c618
Halyna Danylko <danilkogallina@gmail.com>
2024-12-14 16:21:15 +0200

    crreate post function added

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -7,21 +7,23 @@ export const createPost = async (req, res) => {
   try {
     const { text } = req.body;
     let { img } = req.body;
+
     const userId = req.user._id.toString();
-    console.log(req.user._id.toString());
 
     const user = await User.findById(userId);
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
+    if (!user) return res.status(404).json({ message: "User not found" });
 
     if (!text && !img) {
       return res.status(400).json({ error: "Post must have text or image" });
     }
 
     if (img) {
-      const uploadedResponse = await cloudinary.uploader.upload(img);
-      img = uploadedResponse.secure_url;
+      try {
+        const uploadedResponse = await cloudinary.uploader.upload(img);
+        img = uploadedResponse.secure_url;
+      } catch (error) {
+        throw new Error(error.message);
+      }
     }
 
     const newPost = new Post({
@@ -31,7 +33,6 @@ export const createPost = async (req, res) => {
     });
 
     await newPost.save();
-
     res.status(201).json(newPost);
   } catch (error) {
     res.status(500).json({ error: "Internal server error" });
