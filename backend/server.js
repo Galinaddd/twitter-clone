d8ea25d42afa7fd1930e8ee2f8580ce8b0934df5
Halyna Danylko <danilkogallina@gmail.com>
2024-12-14 17:01:04 +0200

    suggested users fetched

--- backend/server.js
+++ backend/server.js
@@ -20,12 +20,12 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true })); //to parse form data (urlencoded)
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/user", userRoutes);
+app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
